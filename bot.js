43d41020508edf8a4f7255e496dab24dbad99aac
streaming-yassine <44115214+fhvdtg@users.noreply.github.com>
2019-05-01 22:12:51 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -53,10 +53,15 @@ client.on('message', msg => {
   if(msg.content === 'دماء')
   msg.reply('صبر')
 });
+
 client.on('message', msg => {
   if(msg.content === '.')
   msg.reply('Welcome To__ C__alifornia Shop ❤ ')
 });
 
+client.on('message', msg => {
+  if(msg.content === ' Welcome To__ C__alifornia Shop ❤ ')
+  msg.reply('Welcome To__ C__alifornia Shop ❤ ')
+});
 
 client.login(process.env.BOT_TOKEN);// Blood Code
