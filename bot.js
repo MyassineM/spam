7c0351ad776fca77227776551baf2d140f19738e
streaming-yassine <44115214+fhvdtg@users.noreply.github.com>
2019-06-17 13:56:28 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,6 +4,7 @@ var prefix = ".";
 var adminprefix = '.'
 
 const developers = ["590157929362227210"]
+
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
