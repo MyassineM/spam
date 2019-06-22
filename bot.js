eb100f7b32ec413fbfe8edee00d4c2bb8fbf56b6
streaming-yassine <44115214+fhvdtg@users.noreply.github.com>
2019-06-22 16:57:17 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,40 +5,10 @@ var adminprefix = '.'
 
 const developers = ["592015903189762081"]
 
-client.on('message', message => {
-    var argresult = message.content.split(` `).slice(1).join(' ');
-      if (!developers.includes(message.author.id)) return;
-      
-  if (message.content.startsWith(adminprefix + 'seto')) {
-    client.user.setGame(argresult);
-      message.channel.send(`**✅   ${argresult}**`)
-  } else 
-     if (message.content === (adminprefix + "leave")) {
-    message.guild.leave();        
-  } else  
-  if (message.content.startsWith(adminprefix + 'setw')) {
-  client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.send(`**✅   ${argresult}**`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'setl')) {
-  client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.send(`**✅   ${argresult}**`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'sets')) {
-    client.user.setGame(argresult, "https://www.twitch.tv/dream");
-      message.channel.send(`**✅**`)
-  }
-  if (message.content.startsWith(adminprefix + 'setname')) {
-  client.user.setUsername(argresult).then
-      message.channel.send(`Changing The Name To ..**${argresult}** `)
-} else
-if (message.content.startsWith(adminprefix + 'setava')) {
-  client.user.setAvatar(argresult);
-    message.channel.send(`Changing The Avatar To :**${argresult}** `);
-}
-});
-
-
-
+client.on('ready', () => {
+        console.log(`back`);
+              client.user.setActivity("🎵 لحن جحيم ", {type: 'LISTENING'});
+      
+      });   // Toxic Code
 
 client.login(process.env.TOKEN);// لا تغير فيها شيء
